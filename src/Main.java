8577ac2d8571d37646f3bedf5028d857d5ec9e06
Mikhail Dukhno <ratercorp@gmail.com>
2023-06-13 13:29:59 +0300

    add code

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13676;
+        int y = 20;
+        int mile = price / y;
+        System.out.println("Количество миль:" + mile);
+
+    }
+}
