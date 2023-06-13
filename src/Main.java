5265ad4ad57c17fe0ae915589626a73fc634e2c0
Mikhail Dukhno <ratercorp@gmail.com>
2023-06-13 14:44:50 +0300

    add changes

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         int price = 13676;
-        int y = 20;
-        int mile = price / y;
+        int coefficient = 20;
+        int mile = price / coefficient;
         System.out.println("Количество миль:" + mile);
 
     }
